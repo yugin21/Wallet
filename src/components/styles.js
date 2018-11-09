12f45b322f95578ede61e76ad491554039472423
yugin21 <yusufalmaududi21@gmail.com>
2018-11-09 13:39:24 +0700

    fix transaction container

--- src/components/styles.js
+++ src/components/styles.js
@@ -109,7 +109,7 @@ export default StyleSheet.create({
         height: '100%',
         position: 'relative',
         paddingLeft: 20,
-        marginBottom: '111.6%',
+        paddingBottom: '87.5%',
     },
     transText:{
         fontSize: 15,
